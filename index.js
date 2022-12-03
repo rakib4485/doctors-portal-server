2a5119eea8da3b90addcf4f33774ebedd3036846
rakib4485 <mdrakibulislam4485@gmail.com>
2022-12-03 21:21:23 +0600

    server site create

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efsdsdy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const appointmentOptionCollection = client.db('doctorsProtal').collection('appointmentOptions');
+        const bookingsCollection = client.db('doctorsProtal').collection('bookings');;
+        app.get('/appointmentOptions', async(req, res)=>{
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        });
+
+        /**
+         * API Naming Convention
+         * app.get('/bookings')
+         * app.get('/bookings/:id')
+         * app.post('/bookings')
+         * app.patch('/booking/:id')
+         * app.delete('/booking/:id')
+         */
+
+        app.post('/bookings', async(req,res) =>{
+            console.log('booking api is called')
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking)
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log())
+
+app.get('/', async (req,res)=>{
+    res.send('doctors portal is running');
+})
+
+app.listen(port, ()=> {
+    console.log(`Doctor portal is running on port ${port}`)
+})
